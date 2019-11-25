bcc6c6d9b52f5e5667aee20ab8ac91a4cc4730e1
uNBEx <osuchkacper@gmail.com>
2019-11-25 09:30:52 +0100

    first excercise done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>  
+    <h1>Clicked {{ counter }} times</h1>
+    <button @click="iAmClicked()">Click me!</button>
   </div>
 </template>
-
+ 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    export default {
+        data() {
+            return {counter:0}
+        },
+        methods: {
+            iAmClicked() {
+                this.counter = this.counter + 1;
+            }
+        }
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    };
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
